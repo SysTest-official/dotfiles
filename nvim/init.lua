3292906a3239857d27f3b94615b1b18b8d6a5090
SysTest <162224716+SysTest-official@users.noreply.github.com>
2026-06-14 04:40:00 -0500

    Add basic Neovim configuration settings

---
+++ nvim/init.lua
@@ -0,0 +1,9 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.termguicolors = true
+
+vim.keymap.set("n", "<leader>w", ":w<CR>")
+vim.keymap.set("n", "<leader>q", ":q<CR>")
